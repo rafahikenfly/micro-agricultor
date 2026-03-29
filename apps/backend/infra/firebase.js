ef19eb8bde39babfd07d839fb17c12de19ba0cb4
Rafael Carvalho Machado <rafael.machado@gmail.com>
2026-03-29 00:36:42 -0300

    Corrigido o acesso do firebase do backend

--- apps/backend/infra/firebase.js
+++ apps/backend/infra/firebase.js
@@ -1,19 +1,16 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+import admin from "firebase-admin";
+import fs from "fs";
 
-const firebase = require("firebase/app");
-require("firebase/firestore");
-require("firebase/auth");
-require("firebase/storage");
-import { firebaseConfig } from "./firebaseConfig.js";
+const serviceAccount = JSON.parse(
+  fs.readFileSync(new URL("./serviceKey.json", import.meta.url))
+);
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "micro-agricultor.firebasestorage.app",
+});
 
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export const storage = firebase.storage();
-export const timestamp = firebase.firestore.FieldValue.serverTimestamp;
-export const nowTimestamp = firebase.firestore.Timestamp.now;
-export const increment = firebase.firestore.FieldValue.increment;
+export const db = admin.firestore();
+export const storage = admin.storage();
+export const timestamp = admin.firestore.FieldValue.serverTimestamp;
+export const increment = admin.firestore.FieldValue.increment;
